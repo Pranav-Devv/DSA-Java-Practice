dd5cb5b5e6aebd05e8b44819784b1bc831fd09ad
Pranav patil <pp4779185@gmail.com>
2026-08-15 23:07:17 +0530

    Time: 0 ms (100%), Space: 43.2 MB (91.11%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,28 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val)
-            {
-               nums[count]=nums[i];
-                count ++;
+
+        int count = 0; // Position where we put valid elements
+
+        // Check every element in the array
+        for (int i = 0; i < nums.length; i++) {
+
+            // If current element is not equal to val
+            if (nums[i] != val) {
+
+                // Copy the valid element to count position
+                nums[count] = nums[i];
+
+                // Move to the next position
+                count++;
             }
-        } 
-        return count; 
+        }
+
+        // Return number of elements left
+        return count;
     }
 }
+ /*i → checks every element
+count → stores valid elements
+!= val → keep the element
+count++ → move to next position
+return count → number of valid elements*/
